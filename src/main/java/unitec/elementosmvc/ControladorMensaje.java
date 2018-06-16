a16583caa227c09973984f25bf569f1e76ba7718
T-107 <T-107@PC182.mshome.net>
2018-06-15 21:42:45 -0500

    15-06-2018

--- src/main/java/unitec/elementosmvc/ControladorMensaje.java
+++ src/main/java/unitec/elementosmvc/ControladorMensaje.java
@@ -5,8 +5,11 @@
  */
 package unitec.elementosmvc;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class ControladorMensaje {
     
-    @GetMapping("/hola")
-    public String algo(){
-        return "hola mundo desde un controlador";
-    }}
+    @Autowired RepositorioMensaje repoMensa;
+    
+//caso a) Buscar todos    
+@GetMapping("/mensaje")
+public List<Mensaje> buscarTodos(){
+    
+    return repoMensa.findAll();
+}
+    
+//caso b)buscar por id
+@GetMapping("/mensaje {id}")//as llaves significan que el ususario necesita introducir un valor 
+    public Mensaje buscarPorId(@PathVariable String id){//siempre que se pone llave se pone
+        //"PathVariable=variable de ruta con variable de tipo id
+        Mensaje mensa = new Mensaje();  
+        return repoMensa.findById(id).get();
+    }
+    
+
+}
